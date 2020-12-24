b9d541c0b6db41eec87735d697c9e4beed390aae
Lucas G <g.brianlucas1993@gmail.com>
2020-12-24 16:55:25 -0300

    added cache

--- api/src/routes/api.js
+++ api/src/routes/api.js
@@ -17,33 +17,38 @@ server.get('/search', (req, res) => {
     //req.params contains route parameters (in the path portion of the URL),
     //and req.query contains the URL query parameters (after the ? in the URL).
     let searchQuery = req.query.q;
-    axios.get(config.apiMercadoLibre + `/sites/MLA/search?q=${searchQuery}`)
-        .then(r => r.json())
-        .then(r => {
-            // Code for handling the response 
-            // the attribute .results is from the documentation of ML api
-            let products = response.results.map(product => {
-                return {
-                    id: product.id,
-                    title: product.title,
-                    price: product.price,
-                    total_price: product.price,
-                    currency_id: product.currency_id,
-                    available_quantity: product.available_quantity,
-                    thumbnail: product.thumbnail,
-                    condition: product.condition,
-                }
-            });
 
-            cache = products;
+    if (cache.hasOwnProperty(searchQuery)) {
+        return res.send(cache[searchQuery]);
+    } else {
+        axios.get(config.apiMercadoLibre + `/sites/MLA/search?q=${searchQuery}`)
+            .then(data => data.json())
+            .then(data => {
+                // Code for handling the response 
+                // the attribute .results is from the documentation of ML api
+                let products = data.results.map(product => {
+                    return {
+                        id: product.id,
+                        title: product.title,
+                        price: product.price,
+                        total_price: product.price,
+                        currency_id: product.currency_id,
+                        available_quantity: product.available_quantity,
+                        thumbnail: product.thumbnail,
+                        condition: product.condition,
+                    }
+                });
+
+            cache[searchQuery] = products;
             res.send(products);
 
-        })
-        .catch(error => {
-            // Code for handling the error 
-            console.log(error);
-        });
-
+            })
+            .catch(error => {
+                // Code for handling the error 
+                console.log(error);
+                res.status(404).send("Error");
+            });
+    }
 });
 
 module.exports = server;
